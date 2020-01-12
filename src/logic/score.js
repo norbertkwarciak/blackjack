7f9a2bd2ab4ee12d1353848ec9e1d8405d6b8d6e
Norbert Kwarciak <norbert.kwarciak@gmail.com>
2020-01-12 16:27:14 +0100

    Multiple aces scenario

--- src/logic/score.js
+++ src/logic/score.js
@@ -1,7 +1,25 @@
+const countSum = items => items
+  .map(i => i.value)
+  .reduce((prev, curr) => prev + curr, 0);
+
 export const getScore = cards => {
-  const score = cards
-    .map(c => c.value)
-    .reduce((prev, curr) => prev + curr, 0);
+  let score;
+  const aces = cards.filter(c => c.label === 'A');
+
+  const FIRST_ACE_VALUE = 11;
+  const OTHER_ACE_VALUE = 1;
+
+  if (aces.length > 1) {
+    const [firstAce, ...restOfAces] = aces;
+    firstAce.value = FIRST_ACE_VALUE;
+    restOfAces.map(ace => ace.value = OTHER_ACE_VALUE);
+
+    const updatedCards = cards.every(c => c.label === 'A')
+      ? [firstAce, ...restOfAces]
+      : [firstAce, ...restOfAces, ...cards.filter(c => c.label !== 'A')];
+
+    score = countSum(updatedCards);
+  } else score = countSum(cards);
 
   return score;
 }
--- src/logic/score.spec.js
+++ src/logic/score.spec.js
@@ -1,13 +1,13 @@
 import { getScore } from './score';
 
 const CARDS = [
-  {value: 8, label: 8, suit: 'spades'},
-  {value: 5, label: 5, suit: 'diamonds'},
-  {value: 10, label: 'K', suit: 'hearts'}
+  {value: 11, label: 'A', suit: 'spades'},
+  {value: 1, label: 'A', suit: 'diamonds'},
+  {value: 5, label: 5, suit: 'hearts'}
 ];
 
 describe('getScore function', () => {
   test('should count the sum of card values', () => {
-    expect(getScore(CARDS)).toBe(23);
+    expect(getScore(CARDS)).toBe(17);
   });
 })
