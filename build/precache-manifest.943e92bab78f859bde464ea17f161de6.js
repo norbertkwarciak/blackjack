ebd3c65ff0f61bc616dc53b3c39542ab10ef6ed3
Norbert Kwarciak <norbert.kwarciak@gmail.com>
2020-01-14 22:09:44 +0100

    Build for deploy

---
+++ build/precache-manifest.943e92bab78f859bde464ea17f161de6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a147ac4cb9e13b494660f4a6e41f466",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70552228ceb234a3a180",
+    "url": "/static/css/main.3788dd29.chunk.css"
+  },
+  {
+    "revision": "2bd25f35415cf03277de",
+    "url": "/static/js/2.beaa0b39.chunk.js"
+  },
+  {
+    "revision": "36587352e7815de0ba225b441f686430",
+    "url": "/static/js/2.beaa0b39.chunk.js.LICENSE"
+  },
+  {
+    "revision": "70552228ceb234a3a180",
+    "url": "/static/js/main.dcd6890c.chunk.js"
+  },
+  {
+    "revision": "56226830fe8acbc160da",
+    "url": "/static/js/runtime-main.3d8f71dc.js"
+  }
+]);
